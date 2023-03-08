eed8433246c655f4b6a905feeec2b6d260281386
TigerStrike6026 <47280945+TigerStrike6026@users.noreply.github.com>
2023-03-08 15:07:24 -0600

    Limelight Tag ID

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.util.LimelightHelpers;
 
 public class Limelight extends SubsystemBase {
     private final NetworkTableInstance _instance = NetworkTableInstance.getDefault();
@@ -21,5 +22,10 @@ public class Limelight extends SubsystemBase {
         var y = robotPoseArray[2];
         var rotation = robotPoseArray[4];
         return new Pose2d(x, y, Rotation2d.fromDegrees(rotation));
+    }
+
+    public double getTagId() {
+        double tagId = LimelightHelpers.getFiducialID(getName());
+        return tagId;
     }
 }
